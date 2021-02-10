391d0a66eb22bba473f4b6fb4c84af7bff0ac59b
vf58bwk4 <alzhu@mail.ru>
2021-02-10 14:57:52 +0300

    First commit after a long time

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,33 @@
 module Main where
+import System.Environment
+import Data.List
+import Data.List.Split
 
 main :: IO ()
+-- main = do
+--   putStrLn "hello world"
+
+-- main = interact id
+-- count s = show (length s) ++ "\n"
+-- main = interact count
+-- main = interact (count . lines)
+-- main = interact (unlines . map reverse . lines)
+
+-- main = do
+--     [inF,outF] <- getArgs
+--     s <- readFile inF
+--     writeFile outF s
+
+convertDDMM2MMDD = 
+    intercalate "/" . (\(dd:mm:xs) -> mm:dd:xs) . splitOn "/"
+
+myReformat = 
+    intercalate ";" . (\(f1:f2:f3:_) = [convertDDMM2MMDD f1,f2,f3]) . splitOn "\t"
+    
 main = do
-  putStrLn "hello world
+    [inF,outF] <- getArgs
+    s <- readFile inF
+    writeFile outF ((unlines . map myReformat . lines) s)
+    
+
+    putStrLn "Done."
